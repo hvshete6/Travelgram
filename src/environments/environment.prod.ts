57074219af4c0932858b31918cb4a0963843c217
Harshvardhan Shete <hvshete6@gmail.com>
2021-01-17 17:27:22 +0530

    Environment ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDhDm2Tx8WXZSo9f1hpCxrNNXEwUB28NX8",
+    authDomain: "travelgram-8e2ac.firebaseapp.com",
+    databaseURL: "https://travelgram-8e2ac.firebaseio.com",
+    projectId: "travelgram-8e2ac",
+    storageBucket: "travelgram-8e2ac.appspot.com",
+    messagingSenderId: "913126865458",
+    appId: "1:913126865458:web:1d35e8dffca65f10561a2b",
+    measurementId: "G-M03M022D0D"
+  },
 };
